d8e30d9707f320e4bf9fb90f46be06b5deec756e
Mikis Woodwinter <mikis.woodwinter@gmail.com>
2019-07-15 22:18:08 -0700

    Add an account
    Signed-off-by: Mikis Woodwinter <mikis.woodwinter@gmail.com>

--- server.js
+++ server.js
@@ -11,4 +11,13 @@ server.get("/api/accounts", async (req, res) => {
     res.json(accounts)
 })
 
+server.post("/api/accounts", async (req, res) => {
+    const [id] = await db.insert(req.body).into("accounts")
+    const createdAccount = await db
+        .select()
+        .table("accounts")
+        .where("id", id)
+    res.json(createdAccount)
+})
+
 module.exports = server

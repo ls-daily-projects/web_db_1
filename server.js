66abf8eee7730d43e1598beca90b2f3050938ca9
Mikis Woodwinter <mikis.woodwinter@gmail.com>
2019-07-15 22:05:11 -0700

    Get all accounts
    Signed-off-by: Mikis Woodwinter <mikis.woodwinter@gmail.com>

--- server.js
+++ server.js
@@ -6,4 +6,9 @@ const server = express()
 
 server.use(express.json())
 
+server.get("/api/accounts", async (req, res) => {
+    const accounts = await db("accounts")
+    res.json(accounts)
+})
+
 module.exports = server
